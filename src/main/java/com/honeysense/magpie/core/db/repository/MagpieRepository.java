8230d719d52f0ba73713548d7e83c5acb8263c92
sumrui <smallrui@foxmail.com>
2021-03-24 12:42:39 +0800

    no message

--- src/main/java/com/honeysense/magpie/core/db/repository/MagpieRepository.java
+++ src/main/java/com/honeysense/magpie/core/db/repository/MagpieRepository.java
@@ -1,6 +1,8 @@
 package com.honeysense.magpie.core.db.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.NoRepositoryBean;
 
+@NoRepositoryBean
 public interface MagpieRepository<T> extends JpaRepository<T, Integer> {
 }
